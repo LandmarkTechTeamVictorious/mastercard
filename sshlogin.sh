909a8167eb3d9125d91659bbe72608300e51bce5
opysanni <opysan@gmail.com>
2022-08-19 02:23:23 +0000

    Added a comment and made a few changes

--- sshlogin.sh
+++ sshlogin.sh
@@ -1,11 +1,13 @@
-echo -ne "Enter Your Exact Username: "
+#this is a simple bash script to login via ssh diretly to your computer/server
+
+echo -ne "Enter your Exact Username: "
 read name
 
 echo -ne "Enter your public ip or DNS: "
 read publicip
 
 echo "This script is for logging into any instance/server"
-echo "***********   RUNNING SCRIPT: sshlignin.sh ************"
+echo "*********** RUNNING SCRIPT: sshlogin.sh ************"
 echo "ssh $name@$publicip"
 
 ssh $name@$publicip
